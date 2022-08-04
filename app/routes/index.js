ce37be8010d65f557e9eb56e0c952592549fb161
v-zdorovcev <v.zdvacew@gmail.com>
2022-08-05 00:43:33 +0700

    add router prefix

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,13 @@
 import { Router } from "express";
+import { loginHandler, profileHandler, registerHandler } from "../controller/auth.js";
+import { registerValidation } from "../lib/validations/auth.js";
+import checkAuth from "../middleware/checkAuth.js";
 
 const router = Router();
+
+// Auth
+router.post("/auth/register", registerValidation, registerHandler);
+router.post("/auth/login", loginHandler);
+router.get("/auth/me", checkAuth, profileHandler);
+
+export default router;
--- app/server.js
+++ app/server.js
@@ -1,9 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation } from "./lib/validations/auth.js";
-import checkAuth from "./middleware/checkAuth.js";
-import { loginHandler, profileHandler, registerHandler } from "./controller/auth.js";
+import router from "./routes/index.js";
 
 const PORT = process.env.PORT || 8000;
 
@@ -19,10 +17,7 @@ mongoose
   });
 
 app.use(express.json());
-
-app.post("/auth/register", registerValidation, registerHandler);
-app.post("/auth/login", loginHandler);
-app.get("/auth/me", checkAuth, profileHandler);
+app.use("/api", router);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
